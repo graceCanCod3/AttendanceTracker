8549071e4e9387bf9aaae15de764e27312e6aa24
graceCanCod3 <gracem@graces-iMac.local>
2024-07-31 15:29:46 -0400

    updated classSession page

--- frontend/tracker-project/src/components/classSession.jsx
+++ frontend/tracker-project/src/components/classSession.jsx
@@ -2,32 +2,37 @@ import axios from 'axios'
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-export default classSession (props) {
+export default function ClassSession() {
   const [sessions, setSessions] = useState([])
   const navigate = useNavigate()
 
   useEffect(() => {
-    getAllVenues()
+    getAllSessions()
   }, [])
 
   const getAllSessions = async () => {
     try {
-      const response = await axios.get('http://127.0.0.1:8000/api/classsessions')
+      const response = await axios.get(`http://127.0.0.1:8000/api/classsessions`)
       setSessions(response.data)
     } catch (error) {
-      console.error('Error getting class session:', error)
+      console.error('Error getting class sessions:', error)
     }
-  }
+  };
+
   const handleSessionClick = (id) => {
     navigate(`/classsessions/${id}`)
   }
 
     return (
-
       <div>
-        <h1>Class Session</h1>
+        <h1>Class Sessions</h1>
+        <ul>
+          {sessions.map(session => (
+            <li key={session.id} onClick={() => handleSessionClick(session.id)}>
+              {session.subject.name} - {session.date}
+            </li>
+          ))}
+        </ul>
       </div>
-    )
-  }
-  
- 
+  )
+}
